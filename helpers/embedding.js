06c518a4a689cb24702a2a87afc157271b415617
Arman Patel <vanixgaming634@gmail.com>
2026-03-04 14:29:43 +0530

    Update embedding model and adjust embedContent parameters

--- helpers/embedding.js
+++ helpers/embedding.js
@@ -1,11 +1,14 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config"; 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
+const model = genAI.getGenerativeModel({ model: "gemini-embedding-001" });
 
 export default async function embed(text) {
     try {
-        const result = await model.embedContent(text)
+        const result = await model.embedContent({
+            content: { parts: [{ text: text }] },
+            outputDimensionality: 768,
+          });
         const vector = result.embedding.values
         return vector
     } catch (error) {
